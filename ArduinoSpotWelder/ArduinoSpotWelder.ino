a1452a55ff483109d782c6adfcf9072de00da599
Yannick Gagne <yannickg@gmail.com>
2022-01-29 21:17:12 -0500

    folder fix + formatting

--- ArduinoSpotWelder/ArduinoSpotWelder.ino
+++ ArduinoSpotWelder/ArduinoSpotWelder.ino
@@ -1,23 +1,18 @@
 /*
-
   Spot Welder Timer Example
 
   Check out the instructable here - http://www.instructables.com/id/DIY-Spot-Welder-From-Microwave/
-    
+
   >>> Before compiling: Please install the u8glib library from below!
-  
+
   Universal 8bit Graphics Library, https://github.com/olikraus/u8glib/
-  
+
   Jack Davies, http://jackdaviesdesign.com <<< >>> http://undergroundengineering.co.uk
-
-  
-  
 */
-
 
 #include "U8glib.h"
 
-U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE|U8G_I2C_OPT_DEV_0);	// I2C / TWI 
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE | U8G_I2C_OPT_DEV_0);	// I2C / TWI
 
 int potPin = 0;   // analog pin used to connect the potentiometer
 int potVal;       // variable to read the value from the analog pin
@@ -30,19 +25,19 @@ int stringWidth = 0;
 void draw(void) {
   // graphic commands to redraw the complete screen
   u8g.setFont(u8g_font_unifont);
-  u8g.drawStr( 14, 10, "JD Spot Weld!"); // rename to personalise ;) 
+  u8g.drawStr( 14, 10, "YG Spot Welder"); // rename to personalise ;)
   u8g.setScale2x2();
-  u8g.setPrintPos(20,20);
+  u8g.setPrintPos(20, 20);
   u8g.print(potVal);
   u8g.undoScale();
   u8g.drawStr( 20, 60, "Miliseconds");
-      
+
 }
 
 void setup(void) {
   // flip screen, if required
   // u8g.setRot180();
-  
+
   // set SPI backup if required
   //u8g.setHardwareBackup(u8g_backup_avr_spi);
 
@@ -57,40 +52,36 @@ void setup(void) {
     u8g.setColorIndex(1);         // pixel on
   }
   else if ( u8g.getMode() == U8G_MODE_HICOLOR ) {
-    u8g.setHiColorByRGB(255,255,255);
+    u8g.setHiColorByRGB(255, 255, 255);
   }
 
-      Serial.begin(9600);
-
-    pinMode(inPin, INPUT);
-    pinMode(ssrPin, OUTPUT);
-
+  Serial.begin(9600);
+  pinMode(inPin, INPUT);
+  pinMode(ssrPin, OUTPUT);
 }
 
 void loop(void) {
   // picture loop
-  u8g.firstPage();  
+  u8g.firstPage();
   do {
     draw();
-  } while( u8g.nextPage() );
-  
+  } while ( u8g.nextPage() );
+
   // rebuild the picture after some delay
   //delay(50);
-            
+
   potVal = analogRead(potPin);                // reads the value of the potentiometer (value between 0 and 1023)
-  potVal = map(potVal, 0, 1023, 10, 500);     // scale it to use it get the right time (value between 10 and 500)
+  potVal = map(potVal, 0, 1023, 10, 750);     // scale it to use it get the right time (value between 10 and 500)
 
   triggerSwitch = digitalRead(inPin);
 
-  Serial.println(triggerSwitch);
+  //Serial.println(triggerSwitch);
 
-
-  if (triggerSwitch == HIGH){
-        digitalWrite(ssrPin, HIGH);
-        delay(potVal);
-        digitalWrite(ssrPin, LOW);
-        delay(1000);
+  if (triggerSwitch == HIGH) {
+    digitalWrite(ssrPin, HIGH);
+    delay(potVal);
+    digitalWrite(ssrPin, LOW);
+    delay(1000);
   }
-  
-}
 
+}
